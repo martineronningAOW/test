d85102942d027e0551fd1e2eb07e2e8e73bb77d9
martineronningAOW <95696100+martineronningAOW@users.noreply.github.com>
2022-03-21 20:34:26 +0100

    Update 4mikkel.py

--- 4mikkel.py
+++ 4mikkel.py
@@ -54,4 +54,30 @@ sim_res_time = wf_model(x, y, # wind turbine positions
 
 total_AEP_time = sim_res_time.aep(with_wake_loss=True).sum().data
 total_AEP_nowake_time = sim_res_time.aep(with_wake_loss=False).sum().data
-sim_res_time.Power.sel(wt=0).plot(figsize=(16,3))
+
+
+Traceback (most recent call last):
+  File "C:\ProgramData\Anaconda3\envs\V1PyWake\lib\site-packages\IPython\core\interactiveshell.py", line 3444, in run_code
+    exec(code_obj, self.user_global_ns, self.user_ns)
+  File "<ipython-input-62-b3a7ec634a0d>", line 1, in <module>
+    total_AEP_nowake_time = sim_res_time.aep(with_wake_loss=False).sum().data
+  File "C:\ProgramData\Anaconda3\envs\V1PyWake\lib\site-packages\py_wake\wind_farm_models\wind_farm_model.py", line 241, in aep
+    power_ilk = self.windFarmModel.windTurbines.power(self.WS.ilk(self.Power.ilk().shape), **self.wt_inputs)
+  File "C:\ProgramData\Anaconda3\envs\V1PyWake\lib\site-packages\py_wake\wind_turbines\_wind_turbines.py", line 80, in power
+    return self.powerCtFunction(ws, run_only=0, **kwargs)
+  File "C:\ProgramData\Anaconda3\envs\V1PyWake\lib\site-packages\py_wake\wind_turbines\power_ct_functions.py", line 173, in __call__
+    power_ct_arr = PowerCtModelContainer.__call__(self, ws, run_only=run_only, **kwargs)
+  File "C:\ProgramData\Anaconda3\envs\V1PyWake\lib\site-packages\py_wake\wind_turbines\power_ct_functions.py", line 71, in __call__
+    return recursive_wrap(0, ws, **kwargs)
+  File "C:\ProgramData\Anaconda3\envs\V1PyWake\lib\site-packages\py_wake\wind_turbines\power_ct_functions.py", line 64, in recursive_wrap
+    return f(lambda ws, model_idx=model_idx + 1, **kwargs: recursive_wrap(model_idx, ws, **kwargs),
+  File "C:\ProgramData\Anaconda3\envs\V1PyWake\lib\site-packages\py_wake\wind_turbines\power_ct_functions.py", line 118, in __call__
+    return f(ws, **kwargs)
+  File "C:\ProgramData\Anaconda3\envs\V1PyWake\lib\site-packages\py_wake\wind_turbines\power_ct_functions.py", line 64, in <lambda>
+    return f(lambda ws, model_idx=model_idx + 1, **kwargs: recursive_wrap(model_idx, ws, **kwargs),
+  File "C:\ProgramData\Anaconda3\envs\V1PyWake\lib\site-packages\py_wake\wind_turbines\power_ct_functions.py", line 68, in recursive_wrap
+    return recursive_wrap(model_idx + 1, ws, **kwargs)
+  File "C:\ProgramData\Anaconda3\envs\V1PyWake\lib\site-packages\py_wake\wind_turbines\power_ct_functions.py", line 70, in recursive_wrap
+    return self.power_ct(ws, **kwargs)
+TypeError: CubePowerSimpleCt._power_ct() got an unexpected keyword argument 'TI'
+
